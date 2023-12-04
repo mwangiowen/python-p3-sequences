8af213a8168fdc2ba6e7d50c491ea606a88fd2d9
mwangiowen <owen31588@gmail.com>
2023-12-05 00:16:33 +0300

    Dn

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib_sequence = [0, 1]
+
+        for i in range(2, length):
+            next_number = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_number)
+
+        print(fib_sequence)
+
+# Test the function
+print_fibonacci(9)
